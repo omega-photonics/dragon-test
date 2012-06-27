7fe3a762fad3b520bad4e6c99d127b8dd0a38030
root <root@c2d-ii>
2012-06-28 02:12:31 +0400

    updated to agree latest firmware and module changes

--- dragon-test.cpp
+++ dragon-test.cpp
@@ -17,25 +17,11 @@
 
 #define DRAGON_DEV_FILENAME "/dev/dragon"
 
-#define START_PULSE_WIDTH 5 // (127 max)
+#define FrameLength 9000
+#define FramesPerBuffer 3
+#define PacketCount (FrameLength*FramesPerBuffer/90)
 
-#define DRAGON_DEFAULT_FRAME_LENGTH 8192	// (8192 max) - real frame point count per channel is 6x larger, but device just counts by 6 points
-#define DRAGON_DEFAULT_FRAME_COUNT 10000
-#define DRAGON_DEFAULT_DAC_DATA 0xFFFFFFFF  //each byte for one of four 8-bit DACs
-
-uint16_t FrameLength = DRAGON_DEFAULT_FRAME_LENGTH; //how many points in one input (and ouput) frame we have
-uint32_t FrameCount =  DRAGON_DEFAULT_FRAME_COUNT; //how many frames we want to sum to get output data
-uint32_t PcieDacData = DRAGON_DEFAULT_DAC_DATA; //DAC data to be set on PCIE device
-
-// get system time in milliseconds
-unsigned long GetTickCount()
-{
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    return (tv.tv_sec*1000+tv.tv_usec/1000);
-}
-
-
+#define USE_BUFFERS 4
 
 //main function, here main PCIE thread runs
 int main(int argc, char** argv)
@@ -43,7 +29,6 @@ int main(int argc, char** argv)
     int DragonDevHandle; //handle for opening PCIE device file
     unsigned char* PcieInputDataShadows[DRAGON_BUFFER_COUNT]; //here we map kernel memory area which PCIE device fills with data via DMA
     unsigned int i, j, k;
-    uint32_t CONFIG_REG_1 = (1<<21) | ((START_PULSE_WIDTH<<13) & 127) | ((FrameLength-1) & 8191);
 
     //open PCIE device
     DragonDevHandle = open(DRAGON_DEV_FILENAME, O_RDWR);
@@ -53,46 +38,40 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    for(i=0; i<DRAGON_BUFFER_COUNT; i++)
+    for(i=0; i<USE_BUFFERS; i++)
     {
         ioctl(DragonDevHandle, DRAGON_REQUEST_BUFFER_NUMBER, i);
-        PcieInputDataShadows[i] = (unsigned char*)mmap(0, DRAGON_BUFFER_SIZE, PROT_READ, MAP_SHARED, DragonDevHandle, 0);
-        ioctl(DragonDevHandle, DRAGON_QUEUE_BUFFER, i);
+        PcieInputDataShadows[i] = (unsigned char*)mmap(0, DRAGON_BUFFER_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, DragonDevHandle, 0);
     }
 
-    ioctl(DragonDevHandle, DRAGON_SET_DAC, PcieDacData);
-
-    ioctl(DragonDevHandle, DRAGON_SET_REG1, CONFIG_REG_1);
-    ioctl(DragonDevHandle, DRAGON_SET_REG2, 1);
+    ioctl(DragonDevHandle, DRAGON_SET_FRAME_LENGTH, FrameLength);
+    ioctl(DragonDevHandle, DRAGON_SET_FRAME_PER_BUFFER_COUNT, FramesPerBuffer);
+    ioctl(DragonDevHandle, DRAGON_START, 0);
     ioctl(DragonDevHandle, DRAGON_START, 1);
-
 
     while(true)
     {
-        for(k=0; k<DRAGON_BUFFER_COUNT; k++)
+//        ioctl(DragonDevHandle, DRAGON_START, 0);
+        for(k=0; k<USE_BUFFERS; k++)
             ioctl(DragonDevHandle, DRAGON_QUEUE_BUFFER, k);
+//        ioctl(DragonDevHandle, DRAGON_START, 1);
 
         usleep(1000000);
 
-        for(k=0; k<DRAGON_BUFFER_COUNT; k++)
+        for(k=0; k<USE_BUFFERS; k++)
         {
             printf("\n\nbuffer: %d", k);
-            i=0;
-            printf("\nfirst pckt: %d,\tsextets: ",
-                   ((0x0F&PcieInputDataShadows[k][i])<<24) | (PcieInputDataShadows[k][i+1]<<16) |
-                   (PcieInputDataShadows[k][i+2]<<8) | PcieInputDataShadows[k][i+3]);
+            for(i=0; i<PacketCount*DRAGON_PACKET_SIZE_BYTES; i+=DRAGON_PACKET_SIZE_BYTES)
+            {
+                printf("\n%d\tpckt:\t%d\tf:%d,\tsextets: ", i/DRAGON_PACKET_SIZE_BYTES,
+                       ((0x0F&PcieInputDataShadows[k][i])<<24) | (PcieInputDataShadows[k][i+1]<<16) |
+                       (PcieInputDataShadows[k][i+2]<<8) | PcieInputDataShadows[k][i+3], (PcieInputDataShadows[k][i]&128)>>7);
 
-            for(j=0; j<15; j++)
-                printf("%d\t", ((PcieInputDataShadows[k][i+4+j*8]&31)<<8) | PcieInputDataShadows[k][i+5+j*8]);
-
-            i=DRAGON_BUFFER_SIZE-DRAGON_PACKET_SIZE_BYTES;
-            printf("\nlast  pckt: %d \t sextets: ",
-                   ((0x0F&PcieInputDataShadows[k][i])<<24) | (PcieInputDataShadows[k][i+1]<<16) |
-                   (PcieInputDataShadows[k][i+2]<<8) | PcieInputDataShadows[k][i+3]);
-
-            for(j=0; j<15; j++)
-                printf("%d\t", ((PcieInputDataShadows[k][i+4+j*8]&31)<<8) | PcieInputDataShadows[k][i+5+j*8]);
+                for(j=0; j<15; j++)
+                    printf("%d\t", ((PcieInputDataShadows[k][i+4+j*8]&31)<<8) | PcieInputDataShadows[k][i+5+j*8]);
+            }
         }
+        puts("");
     }
 
     return 0;
